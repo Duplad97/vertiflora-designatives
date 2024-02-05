764ec7b6741b1daafefd82b6efef0581054da97b
Dávid Deák <david.deak@drb.services>
2024-02-05 12:05:10 +0100

    Create About Us component

--- app/components/about-us.tsx
+++ app/components/about-us.tsx
@@ -1,12 +1,23 @@
+import { LEFT_CONTENT_TEXT, LEFT_CONTENT_TITLE, RIGHT_CONTENT_TEXT } from "../typography/about-us";
+import ArrowRight from "./custom/arrow-right";
+
 export default function AboutUs() {
 
     return (
         <div className="about-us">
 
-            <div className="left-content"></div>
+            <div className="left-content">
+                <div className="title">
+                    <ArrowRight />
+                    <h6>{LEFT_CONTENT_TITLE}</h6>
+                </div>
+                <p>{LEFT_CONTENT_TEXT}</p>
+            </div>
 
-            <div className="right-content"></div>
-            
+            <div className="right-content">
+                <p><span>{RIGHT_CONTENT_TEXT}</span></p>
+            </div>
+
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import AboutUs from "./components/about-us";
 import Header from "./components/header";
 
 export default function Home() {
     return (
         <main>
             <Header />
+            <AboutUs />
         </main>
     );
 }
