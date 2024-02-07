df1b6908660886c5bc292a298a06408df45c698d
Dávid Deák <david.deak@drb.services>
2024-02-07 01:34:54 +0100

    Update next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/vertiflora-designatives",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
